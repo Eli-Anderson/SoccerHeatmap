49edb5380afc2c0e99b9feb162c1b24aa9e5538c
Amir H. E <amir-hossein.ebrahimzadeh@stud.hs-hannover.de>
2019-10-04 13:39:42 +0200

    classes finished

---
+++ backend/logic/Lineup.py
@@ -0,0 +1,11 @@
+class Lineup:
+
+        def __init__(self, lineup, position):
+            self._lineup = lineup
+            self._position = position
+       
+        def getPosition(self, int):
+            return self._position
+       
+        def getPlayer(self, position):
+            return self._int
