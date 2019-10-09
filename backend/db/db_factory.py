b7a2458f901f557f1d97b5591aafb55be833278a
Amir H. E <amir-hossein.ebrahimzadeh@stud.hs-hannover.de>
2019-10-09 15:57:57 +0200

    more db_factory queries...

--- backend/db/db_factory.py
+++ backend/db/db_factory.py
@@ -1,4 +1,4 @@
-from DB_Connection import Connection
+from db_connection import Connection
 from pprint import pprint
 
 class DB_Factory:
@@ -6,11 +6,21 @@ class DB_Factory:
     connection = Connection()
 
     def searchTeam(self, team_name):
-        #String translation in SQL Query
+        # String translation in SQL Query
         statement = 'select long_name from soccer02.team where long_name = \'' + team_name + '\''
+        self.connection.cursor.execute(statement)
+        res = self.connection.cursor.fetchall()
+        pprint(res)
+    
+    def searchTeamsMatches(self, team_name):
+         # String translation in SQL Query
+        statement = 'select team.long_name, match.result from soccer02.team inner join soccer02.match on team.team_id = match.team_hometeam_id where long_name = \'' + team_name + '\''
         self.connection.cursor.execute(statement)
         res = self.connection.cursor.fetchall()
         pprint(res)
 
     def close(self):
         self.connection.close()
+
+db = DB_Factory()
+db.searchTeamsMatches('Tubize')
