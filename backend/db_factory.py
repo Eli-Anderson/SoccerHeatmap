e90a7b53bb9cb2b840dad02b71587de73ebdf83f
Cris <christian.gerner@stud.hs-hannover.de>
2019-10-11 18:49:18 +0200

    tuning search testing for tupel-issue

--- backend/db_factory.py
+++ backend/db_factory.py
@@ -6,12 +6,20 @@ class DB_Factory:
     # This is a classvariable meaning all instances access to the same connection
     connection = Connection()
 
-    def searchTeam(self, team_name):
+    def list_all_teams(self):
+        statement = 'select distinct long_name from soccer02.team'
+        self.connection.cursor.execute(statement)
+        res = self.connection.cursor.fetchall()
+        pprint(res)
+        return res
+
+    def search_team(self, team_name):
         # String translation in SQL Query
         statement = 'select long_name from soccer02.team where long_name = \'' + team_name + '\''
         self.connection.cursor.execute(statement)
         res = self.connection.cursor.fetchall()
         pprint(res)
+        return res
     
     def search_home_teams_matches(self, team_name):
          # String translation in SQL Query
@@ -21,6 +29,7 @@ class DB_Factory:
         self.connection.cursor.execute(statement)
         res = self.connection.cursor.fetchall()
         pprint(res)
+        return res
 
     def search_away_teams_matches(self, team_name):
         # String translation in SQL Query
@@ -30,6 +39,7 @@ class DB_Factory:
         self.connection.cursor.execute(statement)
         res = self.connection.cursor.fetchall()
         pprint(res)
+        return res
 
     def close(self):
         self.connection.close()
--- backend/test.py
+++ backend/test.py
@@ -2,7 +2,31 @@ from db_factory import DB_Factory
 
 db = DB_Factory()
 
-print(db.search_home_teams_matches('Tubize'))
+teams = db.list_all_teams()
+print(teams)
+print(type(teams))
+print(type(teams[0]))
+team = teams[0]
+print(team)
+print(type(team))
+str(team)
+print(team)
+print(type(team))
 
-db.close()
+#team = db.search_team(str(teams[1]))
+#print(teams[0])
+
+"""liste = ['Bayern münchen', 'peter hans', '7']
+print(liste)
+print(type(liste))
+print(liste[0])
+print(type(liste[0]))
+
+liste2 = ('Bayern münchen', 'peter hans', '7')
+print(liste2)
+print(type(liste2))
+print(liste2[0])
+print(type(liste2[0]))"""
+
+#db.close()
 
