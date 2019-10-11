8a709e090eb9904458dd71a6f7b87a9b3bbf8883
Amir H. E <amir-hossein.ebrahimzadeh@stud.hs-hannover.de>
2019-10-11 16:53:17 +0200

    Working on the API

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,15 @@
-#apimethod 1
-#apimethod 2
-#apimethod 3
+# This is the API for the communication between the back-end and the front-end
+# Here are all the functions defined which are to be used.
+
+""" 1. method
+:param root:
+
+"""
+# 2.method: Types of events
+#
+# 3.method: Teams
+#
+# 4.method: Players
+#
+# 5.method: Matches
 #
