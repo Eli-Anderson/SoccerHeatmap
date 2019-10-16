acf932ae14ab40794b2663c03d7e4646d771cf01
Eli-Anderson <ultrasnap@yahoo.com>
2019-10-16 10:23:33 -0400

    Created dummy method for 'fetching' our filtered data.
    Created blank Heatmap component and successfully
    retrieved filtered data inside it
    
    Fixed some weird formatting issues with the Filter components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,96 +1,137 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { TeamSelect } from "./teamSelect";
 import { Grid, makeStyles } from "@material-ui/core";
 import { EventSelect } from "./eventSelect";
 import { PlayerSelect } from "./playerSelect";
 import { useState } from "react";
+import { Heatmap } from "./heatmap";
 
 // create our context. The object inside is not necessary, but may help with knowing what
 // fields we are keeping track of.
 // this will help us manage the state of the app easier than passing props around between components.
 const AppContext = React.createContext({
-  event: null,
-  setEvent: null,
-  team: null,
-  setTeam: null,
-  player: null,
-  setPlayer: null
+    event: null,
+    setEvent: null,
+    team: null,
+    setTeam: null,
+    player: null,
+    setPlayer: null,
+    data: null
 });
 
 // define our styles here. this transforms css styles to a class so it is easier to apply
 const useStyles = makeStyles({
-  app: {
-    margin: 20
-  },
-  content: {
-    marginTop: 30
-  }
+    app: {
+        margin: 20
+    },
+    content: {
+        marginTop: 30
+    }
 });
 
+function getData({ event, team, player }) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            if (!player) {
+                resolve(
+                    {
+                        USA: [
+                            {
+                                eventType: "goal",
+                                player1: "Christiano Ronaldo",
+                                player2: "Eden Hazard",
+                                match: 28192831,
+                                date: "2017-06-13",
+                                time: "37:42",
+                                location: 15
+                            },
+                            {
+                                eventType: "penalty",
+                                player1: "Zlatan Ibrahimovic",
+                                player2: "Robin van Persie",
+                                match: 29281923,
+                                date: "2017-07-18",
+                                time: "17:12",
+                                location: 16
+                            }
+                        ]
+                    }[team]
+                );
+            }
+            resolve();
+        }, Math.random() * 3000);
+    });
+}
+
 function App(props) {
-  const classes = useStyles(props);
-  const [event, setEvent] = useState("");
-  const [team, setTeam] = useState("");
-  const [player, setPlayer] = useState("");
-  return (
-    <div className={"App " + classes.app}>
-      {/* Include the roboto font for MaterialUI */}
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <AppContext.Provider
-        // pass our state and mutators on to whichever children need them
-        value={{
-          event,
-          setEvent,
-          team,
-          setTeam,
-          player,
-          setPlayer
-        }}
-      >
-        <h3>Soccer Analyzer</h3>
-        {/* Grid helps us manage the layout of elements */}
+    const classes = useStyles(props);
+    const [event, setEvent] = useState("");
+    const [team, setTeam] = useState("");
+    const [player, setPlayer] = useState("");
+    const [data, setData] = useState([]);
 
-        <Grid
-          id="filterToolbar"
-          container
-          direction="col"
-          justify="space-between"
-        >
-          <EventSelect />
+    useEffect(() => {
+        if (event && team) {
+            getData({ event, team, player }).then(d => setData(d));
+        }
+    }, [event, team, player]);
 
-          <Grid item xs={4}>
-            <Grid
-              container
-              spacing={5}
-              direction="row"
-              justify="flex-start"
-              alignItems="flex-end"
+    return (
+        <div className={"App " + classes.app}>
+            {/* Include the roboto font for MaterialUI */}
+            <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            />
+            <AppContext.Provider
+                // pass our state and mutators on to whichever children need them
+                value={{
+                    event,
+                    setEvent,
+                    team,
+                    setTeam,
+                    player,
+                    setPlayer,
+                    data
+                }}
             >
-              <Grid item xs={4}>
-                <TeamSelect />
-              </Grid>
+                <h3>Soccer Analyzer</h3>
+                {/* Grid helps us manage the layout of elements */}
 
-              <Grid item xs={4}>
-                <PlayerSelect />
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+                <Grid
+                    id="filterToolbar"
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                >
+                    <Grid item>
+                        <Grid container spacing={5} direction="row">
+                            <Grid item>
+                                <EventSelect />
+                            </Grid>
+                            <Grid item>
+                                <TeamSelect />
+                            </Grid>
+                            <Grid item>
+                                <PlayerSelect />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
 
-        {/* Content body */}
+                {/* Content body */}
 
-        <Grid container className={classes.content}>
-          <Grid item xs={12}>
-            <img src="field.jpg" />
-          </Grid>
-        </Grid>
-      </AppContext.Provider>
-    </div>
-  );
+                <Grid container className={classes.content}>
+                    <Grid item>
+                        <Heatmap />
+                        <img src="field.jpg" />
+                    </Grid>
+                </Grid>
+            </AppContext.Provider>
+        </div>
+    );
 }
 
 export { App, AppContext };
