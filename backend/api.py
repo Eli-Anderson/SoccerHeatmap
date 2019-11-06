97589d6a8f6d50e36a47f5c28003f015ffb73690
amir <amir-hossein.ebrahimzadeh@stud.hs-hannover.de>
2019-11-06 15:00:15 +0100

    All documentation added to API

--- backend/api.py
+++ backend/api.py
@@ -4,72 +4,76 @@ from flask import Flask
 db = DB_Factory()
 app = Flask(__name__)
 
-# This is the API for the communication between the back-end and the front-end
-# Here are all the functions defined which are to be used.
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+This is the API for the communication between the back-end and the front-end.
+Here are all the functions defined which are to be used.
 
-""" method
-:param root:
-:returns:
-"""
-############----METHODS-----################################################
+############################----METHODS-----################################
 #        IMPORTANT COMMENT: All returns will be JSON-strings.              #
 #                           "x" means "done".                              #
 ############################################################################
-# ... for all ...
-# - Searching functionality for the "all"-methods.
-#
-# ... for the events...
-# x- Returns all types of events.
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+""" Returns all types of events.
+:returns: Result as JSON.
+"""
 @app.route("/lists/allEventTypes")
 def allEventTypes():
     dummy = db.list_all_events()
     result = json.dumps(dummy)
     return result 
-#
-# x- Gets a string. Returns a concrete event as an event-object (JSON-String) 
-#   containing at least the following information: 
-#   position, event type, elapsed time and (maybe) comment.
-#
-# ...for the teams...
-# x- Returns all teams.
+
+""" Returns all teams.
+:returns: Result as JSON.
+"""
 @app.route("/lists/allTeams")
 def allTeams():
     dummy = db.list_all_teams()
     result = json.dumps(dummy)
     return result
-# x- Gets a team name as a string. Returns all matches of the team and results
+
+""" Returns all matches of the team and results
+:param team_name: The name of the team as a string.
+:returns: Result as JSON.
+"""
 @app.route("/search/teamsMatches")
 def teamMatches(team_name):
     dummy = db.search_home_teams_matches(team_name)
     result = json.dumps(dummy)
     return result
-# - 
-#
-# ...for the players...
-# x- Returns all players.
+
+""" Returns all players.
+:returns: Result as JSON.
+"""
 @app.route("/lists/allPlayers")
 def allPlayers():
     dummy = db.list_all_players()
     result = json.dumps(dummy)
     return result
 
-#
-# ...for the matches...
-# x- Returns all matches.
+""" Returns all matches.
+:returns: Result as JSON.
+"""
 @app.route("/lists/allMatches")
 def allMatches():
     dummy = db.list_all_matches()
     result = json.dumps(dummy)
     return result 
-# x- Gets a the name of the home team and the away team and the date of the 
-#   played match as a string. Returns players, events and the result of 
-#   the match.
+
+""" Returns players, events and the result of the match.
+:param home_team: Name of the home team.
+:param away_team: Name of the away team.
+:param date: Date of the match.
+:returns: Result as JSON.
+"""
 @app.route("/search/Match")
 def match(home_team, away_team, date):
     dummy = db.match_details(home_team, away_team, date)
     result = json.dumps(dummy)
     return result
-#
-#################################################################################
+
+"""
+Running service.
+"""
 app.run(port=3001, debug = True)
 db.close()
