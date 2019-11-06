4dffcb1177b04a2bc71abde46af730a4c6f6cfc1
amir <amir-hossein.ebrahimzadeh@stud.hs-hannover.de>
2019-11-06 16:06:11 +0100

    added function for Heat-map for all attempts on goal of a player from all data

--- backend/api.py
+++ backend/api.py
@@ -72,6 +72,16 @@ def match(home_team, away_team, date):
     result = json.dumps(dummy)
     return result
 
+""" Heat-map for all attempts on goal of a player from all data
+:param player_name: Name of the player.
+:returns: Result as JSON.
+"""
+@app.route("#TODO")
+def player_heatmap(player_name):
+    dummy = db.player_heatmap(player_name)
+    result = json.dumps(dummy)
+    return result
+
 """
 Running service.
 """
--- backend/db_factory.py
+++ backend/db_factory.py
@@ -8,6 +8,7 @@ class DB_Factory:
 
     # Returns all teams as a string-list by converting the query-tupel-results in a list of strings
     def list_all_teams(self):
+        # String translation in SQL Query
         statement = 'select distinct long_name from soccer02.team'
         self.connection.cursor.execute(statement)
         res_tupel = self.connection.cursor.fetchall()
@@ -16,7 +17,6 @@ class DB_Factory:
         return res_string_list
     
     def search_team(self, team_name):
-        # String translation in SQL Query
         statement = 'select long_name, short_name from soccer02.team where long_name = \'' + team_name + '\''
         self.connection.cursor.execute(statement)
         res_tupel = self.connection.cursor.fetchall()
@@ -34,7 +34,6 @@ class DB_Factory:
         return res_string_list
     
     def list_all_players(self):
-        # String translation in SQL Query REMOVED BIRTHDAY
         statement = 'select name from soccer02.player'
         self.connection.cursor.execute(statement)
         res = self.connection.cursor.fetchall()
@@ -42,7 +41,6 @@ class DB_Factory:
         return res
 
     def list_event_by_name(self, event_type):
-        # String translation in SQL Query
         statement = 'select * from soccer02.matchevent ' \
                     'where event_type = \'' + event_type + '\''
         self.connection.cursor.execute(statement)
@@ -51,7 +49,6 @@ class DB_Factory:
         return res
 
     def specific_event(self, event_id):
-        # String translation in SQL Query
         statement = 'select pos_x, pos_y, event_type, elapsed, "comment" from soccer02.matchevent ' \
                     'where matchevent_id = ' + event_id
         pprint(statement)
@@ -73,7 +70,6 @@ class DB_Factory:
         return res_string_list
     # TODO
     def match_details(self, home_team_name, away_team_name, date):
-        # String translation in SQL Query
         statement = 'select player.name, match.result from soccer02.player, soccer02.match ' \
                     'inner join soccer02.match on team.team_id = match.team_awayteam_id ' \
                     'where long_name = \'' + home_team_name + '\''
@@ -83,7 +79,6 @@ class DB_Factory:
         return res
 
     def search_home_teams_matches(self, team_name):
-         # String translation in SQL Query
         statement = 'select team.long_name, match.result from soccer02.team ' \
                     'inner join soccer02.match on team.team_id = match.team_hometeam_id ' \
                     'where long_name = \'' + team_name + '\''
@@ -93,10 +88,20 @@ class DB_Factory:
         return res
 
     def search_away_teams_matches(self, team_name):
-        # String translation in SQL Query
         statement = 'select team.long_name, match.result from soccer02.team ' \
                     'inner join soccer02.match on team.team_id = match.team_awayteam_id ' \
                     'where long_name = \'' + team_name + '\''
+        self.connection.cursor.execute(statement)
+        res = self.connection.cursor.fetchall()
+        pprint(res)
+        return res
+    
+    # Heat-map for all attempts on goal of a player from all data
+    def player_heatmap(self, player_name):
+        statement = 'select * ' \
+                    'from soccer02.player ' \
+                    'join soccer02.matchevent on soccer02.player.player_id = soccer02.matchevent.player_player_id ' \
+                    'where soccer02.player.name like \'' + player_name + '\''
         self.connection.cursor.execute(statement)
         res = self.connection.cursor.fetchall()
         pprint(res)
