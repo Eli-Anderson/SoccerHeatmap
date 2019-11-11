49189bb5ee061af99d1f08c3237eca912ac96abe
Eli-Anderson <ultrasnap@yahoo.com>
2019-11-10 21:23:09 -0500

    Updated playerSelect to use props

--- frontend/src/playerSelect.jsx
+++ frontend/src/playerSelect.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import {
     Select,
     MenuItem,
@@ -7,20 +7,6 @@ import {
     FormControl
 } from "@material-ui/core";
 import { AppContext } from "./App";
-
-const dummyFetch = team => {
-    const teamPlayers = {
-        England: ["Christiano Ronaldo", "Eden Hazard", "Mohamed Salah"],
-        France: ["Andres Iniesta", "Zlatan Ibrahimovic", "Radamel Falcao"],
-        USA: ["Andrea Pirlo", "Robin van Persie", "Yaya Toure"],
-        Spain: ["Edinson Cavani", "Sergio Aguero", "Iker Casillas"]
-    };
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(teamPlayers[team]);
-        }, Math.random() * 3000);
-    });
-};
 
 // define our styles here. this transforms css styles to a class so it is easier to apply
 const useStyles = makeStyles({
@@ -36,30 +22,13 @@ export const PlayerSelect = props => {
     const { team, player, setPlayer } = useContext(AppContext);
     const classes = useStyles(props);
 
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    // this will run only on the initial render
-    useEffect(() => {
-        if (team) {
-            // fetch data here
-            setLoading(true);
-            dummyFetch(team)
-                .then(setData)
-                .then(() => setLoading(false));
-        }
-    }, [team]);
-
     return (
         <div>
             <FormControl>
                 {/* if a team is selected, show 'Player', otherwise show 'Select a team' */}
-                <InputLabel htmlFor="playerSelect">
-                    {team ? "Player" : "Select a team"}
-                </InputLabel>
+                <InputLabel htmlFor="playerSelect">Player</InputLabel>
                 <Select
                     id="playerSelect"
-                    disabled={!team}
                     className={classes.select}
                     value={player}
                     onChange={ev => setPlayer(ev.target.value)}
@@ -68,14 +37,14 @@ export const PlayerSelect = props => {
                         All
                     </MenuItem>
                     {/* if loading, show an item to display this */}
-                    {loading ? (
+                    {props.loading ? (
                         <MenuItem disabled key="loading">
                             Loading...
                         </MenuItem>
                     ) : (
                         // otherwise just display the player names
-                        data.map(x => (
-                            <MenuItem key={x} value={x}>
+                        props.players.slice(0, 60).map((x, i) => (
+                            <MenuItem key={i} value={x}>
                                 {x}
                             </MenuItem>
                         ))
