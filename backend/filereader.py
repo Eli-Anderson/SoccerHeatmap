e8f2484fdae36daa4ad6838fc5eb497fcebb35ac
Cris <christian.gerner@stud.hs-hannover.de>
2019-12-10 18:53:57 +0100

    more testings in preprocessing

--- backend/filereader.py
+++ backend/filereader.py
@@ -1,10 +1,35 @@
 # Read in the file
-with open('./backend/test.txt', 'r') as file :
+with open('./test_raw.txt', 'r') as file :
   filedata = file.read()
 
 # Replace the target string
-filedata = filedata.replace('@', 'blaba')
+filedata = filedata.replace('(', '')
+filedata = filedata.replace(')', '')
+filedata = filedata.replace('None', '0')
+filedata = filedata.replace('y', '1')
+filedata = filedata.replace('y2', '2')
+filedata = filedata.replace('r', '3')
 
 # Write the file out again
-with open('./backend/test.txt', 'w') as file:
+with open('./backend/test_formattet.txt', 'w') as file:
   file.write(filedata)
+
+print("test_done")
+
+# Read in the file
+with open('./training_raw.txt', 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('(', '')
+filedata = filedata.replace(')', '')
+filedata = filedata.replace('None', '0')
+filedata = filedata.replace('y', '1')
+filedata = filedata.replace('y2', '2')
+filedata = filedata.replace('r', '3')
+
+# Write the file out again
+with open('./backend/training_formattet.txt', 'w') as file:
+  file.write(filedata)
+
+print("training_done")
--- backend/ml_pull_db_data.py
+++ backend/ml_pull_db_data.py
@@ -5,17 +5,13 @@ db = DB_Factory()
 #raw_list_training = db.foulcommits_training_data()
 raw_list_test = db.foulcommits_testing_data()
 
-print(str(raw_list_test[0]))
+#print(str(raw_list_test[0]))
 
+output_test = open('test_raw.txt', 'w')
 for data in raw_list_test:
-    for item in data:
+    output_test.writelines(str(data))
+    output_test.write("\n")
 
-        ['0' if item is None else item for item in data]
-
-
-#output_test = open('test_raw.txt', 'w')
-#for data in raw_list_test:
-#    output_test.writelines(str(data))
-#    output_test.write("\n")
-
-#output_test.close()
+output_test.close()
+print("done")
+db.close
