9eb13c7126430c447b963f770ee10f6ebba4cc31
Amir H. E <amir-hossein.ebrahimzadeh@stud.hs-hannover.de>
2019-12-11 14:26:14 +0100

    Final Sprint: Added module for conversion to a NumPyArray

---
+++ backend/magic_testy.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# XTrain = open('./backend/training_formattet.txt','r')
+# yTrain = open('./backend/training_label.txt','r')
+# XTest = open('./backend/test_formattet_feature.txt','r')
+# yTest = open('./backend/test_label.txt','r')
+
+# XTrain = np.array(XTrain)
+# yTrain = np.array(yTrain)
+# XTest = np.array(XTest)
+# yTest = np.array(yTest)
+
+# np.reshape(XTrain, (81159, 11))
+# np.reshape(yTrain, (24951, 11))
+
+xTrain = np.genfromtxt(fname = "./backend/training_formattet.txt", dtype='str')
+print("############ xTrain ############")
+print(xTrain)
+print("############ xTrain END ############")
+
+yTrain = np.genfromtxt(fname = "./backend/training_label.txt", dtype='str')
+print("############ yTrain ############")
+print(yTrain)
+print("############ yTrain END ############")
+
+xTest = np.genfromtxt(fname = "./backend/test_formattet_feature.txt", dtype='str')
+print("############ xTest ############")
+print(xTest)
+print("############ xTest END ############")
+
+yTest = np.genfromtxt(fname = "./backend/test_label.txt", dtype='str')
+print("############ yTest ############")
+print(yTest)
+print("############ yTest END ############")
