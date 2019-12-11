92ff86df4637acce7b4a13bea80b10a3cfffb50a
Cris <christian.gerner@stud.hs-hannover.de>
2019-12-11 18:09:33 +0100

    @A.H.E. #Last Sprint: num_gen refactored

--- backend/num_gen.py
+++ backend/num_gen.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 f = open('./backend/randnum.txt', 'w')
+f2 = open('./backend/randnum_80.txt', 'w')
+f3 = open('./backend/randnum_20.txt', 'w')
 
 for i in range(100000):
     f.write(str(np.random.choice(np.arange(0,4), p=[0.7, 0.2, 0.05, 0.05])) + '\n')
+
+for i in range(80000):
+    f2.write(str(np.random.choice(np.arange(0,4), p=[0.7, 0.2, 0.05, 0.05])) + '\n')
+
+for i in range(20000):
+    f3.write(str(np.random.choice(np.arange(0,4), p=[0.7, 0.2, 0.05, 0.05])) + '\n')
