040a23312c169a1cdb252c47626117e5f315e1de
Cris <christian.gerner@stud.hs-hannover.de>
2019-12-11 13:00:55 +0100

    work in progress

--- backend/magic.py
+++ backend/magic.py
@@ -3,21 +3,30 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
 
-XTrain = open('','r')
-yTrain = open('','r')
-XTest = open('','r')
-yTest = open('','r')
+from keras.datasets import mnist
 
-lines_of_input = 1000
+#XTrain = open('','r')
+#yTrain = open('','r')
+#XTest = open('','r')
+#yTest = open('','r')
+
+(XTrain, yTrain), (XTest, yTest) = mnist.load_data()
+
+XTrain = XTrain.reshape(60000, 784)
+XTest = XTest.reshape(10000, 784)
+XTrain = XTrain/255
+XTest = XTest/255
+
+lines_of_input = 784
+output_vector = 10
 
 #no Scaling due to the big differenze between low and high values
-YTrain = np_utils.to_categorical(yTrain, 4)
-YTest = np_utils.to_categorical(yTest, 4)
+YTrain = np_utils.to_categorical(yTrain, output_vector)
+YTest = np_utils.to_categorical(yTest, output_vector)
 
 magic = Sequential()
-magic.add(Dense(80, input_dim=lines_of_input), activ
-='relu')
-magic.add(Dense(40, activation='relu'))
+magic.add(Dense(80, input_dim=lines_of_input, activation='relu'))
+magic.add(Dense(40,activation='relu'))
 magic.add(Dense(10, activation='sigmoid'))
 magic.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 magic.fit(XTrain, YTrain, epochs=15, verbose=True)
@@ -26,6 +35,3 @@ yP = magic.predict(XTest)
 error = np.flatnonzero(np.argmax(yP, axis=1)-yTest)
 
 print('Correct: %.2f%% '%(100-error.shape[0]/100))
-
-
-
