c099b8ffc0cb172e54fe6f3aa1180896ec1919ae
Cris <christian.gerner@stud.hs-hannover.de>
2019-12-11 17:33:57 +0100

    final

--- backend/filereader.py
+++ backend/filereader.py
@@ -1,6 +1,6 @@
 import numpy as np
 # Read in the file
-with open('./test_raw.txt', 'r') as file :
+with open('./datax.txt', 'r') as file :
   filedata = file.read()
 
 # Replace the target string
@@ -13,16 +13,16 @@ filedata = filedata.replace('r', '3')
 
 
 # Write the file out again
-with open('./backend/test_formattet_feature.txt', 'w') as file:
+with open('./backend/datax_f.txt', 'w') as file:
   file.write(filedata)
 
 
-print(len(open('./backend/test_formattet_feature.txt', 'r').readlines()))
+
 print("test_done")
 
 
 # Read in the file
-with open('./training_raw.txt', 'r') as file :
+with open('./datay.txt', 'r') as file :
   filedata = file.read()
 
 # Replace the target string
@@ -32,10 +32,11 @@ filedata = filedata.replace('None', '0')
 filedata = filedata.replace('y', '1')
 filedata = filedata.replace('y2', '2')
 filedata = filedata.replace('r', '3')
+filedata = filedata.replace(',', '')
+filedata = filedata.replace('\'', '')
 
 # Write the file out again
-with open('./backend/training_formattet.txt', 'w') as file:
+with open('./backend/datay_f.txt', 'w') as file:
   file.write(filedata)
 
-print(len(open('./backend/training_formattet.txt', 'r').readlines()))
 print("training_done")
